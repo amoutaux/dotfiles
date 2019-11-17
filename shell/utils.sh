80a3223819fb67eff8b22a963c4acb13782506eb
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2019-11-17 21:26:42 +0100

    utils.sh: add methods to easier bash scripting

---
+++ shell/utils.sh
@@ -0,0 +1,75 @@
+#A bunch of methods to make bash scripting easier.
+
+
+#Set colors used below
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+purple=$(tput setaf 171)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+tan=$(tput setaf 3)
+blue=$(tput setaf 38)
+
+# Custom printf for headers and logging
+e_header() {
+    printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"
+}
+
+e_arrow() {
+    printf "➜ $@\n"
+}
+
+e_success() {
+    printf "${green}✔ %s${reset}\n" "$@"
+}
+
+e_error() {
+    printf "${red}✖ %s${reset}\n" "$@"
+}
+
+e_warning() {
+    printf "${tan}➜ %s${reset}\n" "$@"
+}
+
+e_underline() {
+    printf "${underline}${bold}%s${reset}\n" "$@"
+}
+
+e_bold() {
+    printf "${bold}%s${reset}\n" "$@"
+}
+
+e_note() {
+    printf "${underline}${bold}${blue}Note:${reset}  ${blue}%s${reset}\n" "$@"
+}
+
+e_info() {
+  printf "%s\n" "$@"
+}
+
+# Ask user confirmation after displaying first argument
+seek_confirmation() {
+  printf "\n${bold}$@${reset}"
+  read -p " (y/n) " -n 1
+  printf "\n"
+}
+
+# Test whether $REPLY ends with 'Y' or 'y'
+is_confirmed() {
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+      return 0
+    fi
+    return 1
+}
+
+# Test whether command is available using 'type'. Used to check for package
+# presence.
+type_exists() {
+    if [ $(type -P $1) ]; then
+      return 0
+    fi
+    return 1
+}
+
