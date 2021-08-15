ee01809752f7fc4696a82776db5466f2c7ff2009
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2021-08-15 23:23:51 +0200

    install.sh: remove macos-related stuff

--- install.sh
+++ install.sh
@@ -60,31 +60,62 @@ fi
 # source utils since they are needed here
 source "$DOTFILES_DIR/shell/utils.sh"
 
+e_header "Dotfiles installation"
+
+setup_apt() {
+
+    e_header "apt update && apt upgrade..."
+    sudo apt update && sudo apt upgrade
+
+    e_bold "Installing build-essential & software-properties-common packages"
+    local -a packages=(
+        'build-essential'
+        'software-properties-common'
+    )
+    # Install all packages
+    sudo apt install -y "$(printf "%s " "${packages[@]}")"
+}
+
 install_packages() {
 
     if [[ ! ($packages || $all) ]]; then
         return
     fi
 
+    setup_apt
+
     local -a packages=(
         'bat'
         'eza'
         'git'
+        'gnome-shell-extension-autohidetopbar'
+        'gnome-shell-extension-caffeine'
+        'gnome-shell-extension-gsconnect'
+        'gnome-shell-extension-remove-dropdown-arrows'
+        'gnome-shell-extensions'
+        'gnome-tweaks'
         'htop'
         'jq'
         'most'
         'neovim'
         'npm'
+        'nodejs'
         'python3'
+        'python3-pip' # pip comes along with python3 on mac
         'ripgrep'
         'task'
+        'terminator'
         'tig'
         'timewarrior'
         'tldr'
         'tmux'
         'tree'
+        'xclip'
         'zsh'
     )
+
+    # WARNING: It is important for xclip that xquartz is installed first
+    setup_apt
 
     e_header "Installing packages from script..."
     # deno is needed by some neovim plugins
@@ -93,9 +124,8 @@ install_packages() {
     curl -LsSf https://astral.sh/uv/install.sh | bash
 
     e_header "Installing packages using package manager..."
-    read -r -p "Package installation command (ex: 'apt install'): " cmd
     for package in "${packages[@]}"; do
-        $cmd "$package" || e_warning "$package installation failed"
+        sudo apt install -y "$package" || e_warning "$package installation failed"
     done
 }
 
