d05b13a987db542ca56dfa5c664283bf389e80e9
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2022-05-03 15:34:29 +0200

    install.sh: fix cask and update packages

--- install.sh
+++ install.sh
@@ -107,7 +107,9 @@ install_packages() {
     local -a brew_cask=(
         'google-chrome'
         'iterm2'
-        'macdown'
+        'brave-browser'
+        'fork'
+        'obsidian'
         'raycast'
         'xquartz'
     )
@@ -117,7 +119,7 @@ install_packages() {
     cmd="brew install"
     e_header "Installing brew cask packages..."
     for package in "${brew_cask[@]}"; do
-        brew cask install "$package" || e_warning "$package install failed."
+        brew install --cask "$package" || e_warning "$package install failed."
     done
     # there's a space in the 'package name' thus it can't be in the loop
     brew install --HEAD universal-ctags/universal-ctags/universal-ctags
