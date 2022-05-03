d03b4d834c1ae29c365349524958dbff8d182837
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2022-05-03 15:34:29 +0200

    install.sh: fix cask and update packages

--- install.sh
+++ install.sh
@@ -97,7 +97,9 @@ install_packages() {
     local -a brew_cask=(
         'google-chrome'
         'iterm2'
-        'macdown'
+        'brave-browser'
+        'fork'
+        'obsidian'
         'raycast'
         'xquartz'
     )
@@ -116,7 +118,7 @@ install_packages() {
 
     e_header "Installing brew cask packages..."
     for package in "${brew_cask[@]}"; do
-        brew cask install "$package" || e_warning "$package install failed."
+        brew install --cask "$package" || e_warning "$package install failed."
     done
 }
 
