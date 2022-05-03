a3b08923a9951a7005927f1736de4eb3a43cbfdf
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2022-05-03 15:34:29 +0200

    install.sh: fix cask and update packages

--- install.sh
+++ install.sh
@@ -107,7 +107,9 @@ install_packages() {
     local -a brew_cask=(
         'google-chrome'
         'iterm2'
-        'macdown'
+        'brave-browser'
+        'fork'
+        'obsidian'
         'raycast'
         'xquartz'
     )
@@ -119,7 +121,7 @@ install_packages() {
 
     e_header "Installing brew cask packages..."
     for package in "${brew_cask[@]}"; do
-        brew cask install "$package" || e_warning "$package install failed."
+        brew install --cask "$package" || e_warning "$package install failed."
     done
 
     e_header "Installing Pyenv..."
