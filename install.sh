71eb54bc8e667a836dbb280d49b6a4d8cdaa71e6
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2022-12-06 17:20:50 +0100

    install.sh: dl linux tarball instead of master

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 set -e
 
 DOTFILES_DIR=$HOME/dotfiles
-DOTFILES_TARBALL_URL="https://www.github.com/amoutaux/dotfiles/tarball/master"
+DOTFILES_TARBALL_URL="https://www.github.com/amoutaux/dotfiles/tarball/linux"
 DOTFILES_GIT_REMOTE="git@github.com/amoutaux/dotfiles.git"
 
 # Options
