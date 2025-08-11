1f1bf813ba9b42843b61a13adb8689b0cb9ee47a
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-08-11 16:31:27 +0200

    nvim: fix nvim-treesitter runtimepath

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,5 +6,7 @@ return {
   opts = {
     auto_install = true,
     ensure_installed = { "c", "javascript", "lua", "python", "vimdoc" },
+    -- Wouldn't be added to runtime path if not explicitly specified ¯\_(ツ)_/¯
+    install_dir = vim.fn.stdpath("data") .. "/site",
   },
 }
