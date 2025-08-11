363c1227ce94af600ebafa99161f174996924934
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-08-11 15:46:12 +0200

    nvim/lazy.lua: disable Lazy updates notifications

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -4,7 +4,8 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out =
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
@@ -34,6 +35,6 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "PaperColor" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
   rocks = { enabled = false },
 })
