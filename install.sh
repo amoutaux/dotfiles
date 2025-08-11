689fa1ec824b3ec90903a2cebeec68f87df4fcba
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-08-11 16:59:47 +0200

    install.sh: use pinentry-mac
    Need a graphical interface for GPG prompts within neovim.

--- install.sh
+++ install.sh
@@ -93,6 +93,7 @@ install_packages() {
         'neovim'
         'npm'
         'node'
+        'pinentry-mac'
         'python3'
         'ripgrep'
         'task'
@@ -289,6 +290,8 @@ create_symlinks() {
     # nvim
     ln -nsf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
     ln -nsf "$DOTFILES_DIR/nvim/tern-config" "$HOME/.tern-config"
+    # pinentry
+    ln -nsf "$(brew --prefix)/bin/pinentry-mac" "$(brew --prefix)/bin/pinentry"
     # shell
     ln -nsf "$DOTFILES_DIR/shell/utils.sh" "$HOME/.utils.sh"
     # taskwarrior
