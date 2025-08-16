26da2fa9664ead1690bb8b0460af60ca3499b13f
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-08-16 11:17:42 +0200

    nvim/colorscheme.lua: darker greys & whitespaces

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,16 +7,19 @@ return {
       light0_hard = "#fcfcfc", -- Closer to white, very subtle off-white
       light0 = "#ffffff", -- Pure white, often used for main background
       light0_soft = "#f8f8f8", -- Slightly off-white, brighter than original
-      light1 = "#f0f0f0", -- Light grey, brighter than original
-      light2 = "#e8e8e8", -- Medium light grey, brighter than original
-      light3 = "#d0d0d0", -- Darker light grey, brighter than original
-      light4 = "#c0c0c0", -- Even darker light grey, brighter than original        },
+      light1 = "#d0d0d0",
+      light2 = "#a8a8a8",
+      light3 = "#888888",
+      light4 = "#606060",
     },
   },
   config = function(_, opts)
     require("gruvbox").setup(opts)
     vim.cmd([[colorscheme gruvbox]])
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "white" })
+    -- Darker whitespaces
+    vim.api.nvim_set_hl(0, "hitespace", { fg = opts.palette_overrides.light4 })
+    -- Make floating windows background
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = opts.palette_overrides.light0_soft })
     -- Overwrite default diff colors
     vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#d5f5dc" })
     vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#ffe4e1" })
