295684e94ec12d5ffa13bd33d15d506d26bcca2e
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-08-17 01:03:55 +0200

    nvim/delimitmate.lua: better brackets management

--- nvim/lua/plugins/delimitmate.lua
+++ nvim/lua/plugins/delimitmate.lua
@@ -1,4 +1,10 @@
 return {
   "Raimondi/delimitMate",
   branch = "master",
+  init = function()
+    vim.g.delimitMate_expand_cr = 1 -- Smart <CR> handling between brackets
+    vim.g.delimitMate_expand_space = 1 -- Optional: add space between brackets
+    vim.g.delimitMate_smart_quotes = 1 -- Handle quotes smartly
+    vim.g.delimitMate_autoclose = 1 -- Autoclose brackets
+  end,
 }
