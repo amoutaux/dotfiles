9b88e32af365124306b2b444688b8a2287178ecd
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-11-03 10:26:37 +0100

    nvim/blink.lua: do not enter suggestions automatically

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -38,7 +38,7 @@ return {
 
     -- (Default) Only show the documentation popup when manually triggered
     --completion = { documentation = { auto_show = false } },
-    completion = { list = { selection = { auto_insert = true } } },
+    completion = { list = { selection = { preselect = false, auto_insert = true } } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
