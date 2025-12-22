81c0fa2bf075e2f76acf50bfb41a1729b2b284bf
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2025-12-22 14:17:41 +0100

    nvim/diagnostics.lua: enable virtual_text and underline

--- nvim/lua/config/diagnostics.lua
+++ nvim/lua/config/diagnostics.lua
@@ -38,8 +38,8 @@ vim.diagnostic.handlers["my/debug_diagnostics"] = {
 
 vim.diagnostic.config({
   ["my/debug_diagnostics"] = true,
-  underline = false,
-  virtual_lines = {
+  underline = true,
+  virtual_text = {
     -- show severity icons as prefixes.
     prefix = function(diagnostic)
       return diagnostic_icons[vim.diagnostic.severity[diagnostic.severity]] .. " "
