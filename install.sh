34284eab325f8ae3a820f7379653004222ad44a0
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2026-07-16 18:29:49 +0200

    Revert "{install.sh,zsh}: install mise"
    This reverts commit 2afbfb36aca3f8fa9b04ca82735f50365e13ff56.

--- install.sh
+++ install.sh
@@ -90,8 +90,6 @@ install_packages() {
     e_header "Installing packages..."
     # deno is needed by some neovim plugins
     curl -fsSL https://deno.land/install.sh | sh
-    # https://mise.jdx.dev/getting-started.html
-    curl https://mise.run | sh
 
     read -r -p "Package installation command (ex: 'apt install'): " cmd
     for package in "${packages[@]}"; do
@@ -222,8 +220,6 @@ create_symlinks() {
     ln -nsf "$DOTFILES_DIR/bepo/lesskey" "$HOME/.lesskey"
     # markdownlint
     ln -nsf "$DOTFILES_DIR/nvim/markdownlint.yaml" "$HOME/.markdownlint.yaml"
-    # mise
-    ln -nsf "$DOTFILES_DIR/mise" "$HOME/.config/mise"
     # nvim
     ln -nsf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
     ln -nsf "$DOTFILES_DIR/nvim/tern-config" "$HOME/.tern-config"
