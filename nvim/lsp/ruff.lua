612e61824327556f6d0032c303a8b2af3d91aa58
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2026-08-04 14:28:49 +0200

    nvim/ruff.lua: remove empty settings
    This seem to prevent ruff from starting properly.

--- nvim/lsp/ruff.lua
+++ nvim/lsp/ruff.lua
@@ -2,9 +2,9 @@ return {
   cmd = { 'ruff', 'server' },
   filetypes = { 'python' },
   root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
-  init_options = {
-    settings = {
-      -- Ruff language server settings go here
-    }
-  }
+  -- init_options = {
+    -- settings = {
+      -- -- Ruff language server settings go here
+    -- }
+  -- }
 }
