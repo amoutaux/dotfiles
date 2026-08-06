f266e6039a3eee17bbf648e18562a175d241c347
Aurélien Moutaux <aurelien.moutaux@gadz.org>
2026-08-06 12:27:19 +0200

    nvim/no-neck-pain.lua: resize with repeatable keymaps

--- nvim/lua/plugins/no-neck-pain.lua
+++ nvim/lua/plugins/no-neck-pain.lua
@@ -1,12 +1,32 @@
 return {
   "shortcuts/no-neck-pain.nvim",
   lazy = false,
+  keys = {
+    { "<leader>np", "<cmd>NoNeckPain<cr>" },
+  },
   opts = {
     autocmds = {
-      enableOnVimEnter = "safe" -- fixes `nvim -d` opening 3-way diff view
-    }
+      enableOnVimEnter = "safe", -- fixes `nvim -d` opening 3-way diff view
+    },
+    disableOnLastBuffer = false,
+    mappings = {
+      enabled = true,
+    },
+    width = 120,
   },
-  keys = {
-    { "<leader>mi", "<cmd>NoNeckPain<cr>"},
-  },
+  config = function(_, opts)
+    local nnp = require("no-neck-pain")
+    nnp.setup(opts)
+
+    local repeatable = require("helpers.repeatable_keymap")
+    repeatable.keymap_set_repeatable("n", "<leader>np+", function()
+      local current_width = vim.api.nvim_win_get_width(0)
+      nnp.resize(current_width + 5)
+    end)
+    repeatable.keymap_set_repeatable("n", "<leader>np-", function()
+      local current_width = vim.api.nvim_win_get_width(0)
+      nnp.resize(current_width - 5)
+    end)
+
+  end,
 }
